01ab45c6db33f5545d3101e843e6f9ea20f64085
DenysBorozenets <borozenetsdenysforme@gmail.com>
2021-09-16 17:24:28 +0300

    add methods

--- src/main/java/com/denis/Car.java
+++ src/main/java/com/denis/Car.java
@@ -1,11 +1,19 @@
 package com.denis;
 
-public class Car {
+import com.denis.interfaces.Battery;
+import com.denis.interfaces.CarDriver;
+import com.denis.interfaces.Tanker;
+
+public class Car implements Battery, CarDriver, Tanker {
     public enum Transmission{MECHANICAL , AUTOMATIC, ROBOT};
     private String model;
     private String number;
     private Transmission transmission;
     private DriverDoc driverDoc;
+
+    boolean isMove = false;
+    boolean isRefuel = false;
+    boolean isOpen = false;
 
     public Car(){
 
@@ -17,6 +25,58 @@ public class Car {
         this.transmission = transmission;
         this.driverDoc = driverDoc;
     }
+
+    @Override
+    public double getWeight(){
+        return 2000;
+    }
+
+    @Override
+    public int getPower() {
+        return 1500;
+    }
+
+    @Override
+    public void move() {
+        if (isRefuel) {
+            System.out.println("Can`t go");
+        } else {
+            System.out.println("Can go");
+        }
+    }
+
+    @Override
+    public void stop() {
+        isMove = false;
+        System.out.println("Stop");
+    }
+
+    @Override
+    public void refuel() {
+        if(!isRefuel){
+            System.out.println("Need fot refuel");
+        } else {
+            System.out.println("Do not need this action");
+        }
+    }
+
+    public void openLid(){
+        if (isMove) {
+            System.out.println("You can`t refuel");
+        } else {
+            System.out.println("You can");
+        }
+    }
+
+    public void closeLid() {
+        if (isMove) {
+            System.out.println("You can`t close");
+        } else {
+            isOpen = true;
+            System.out.println("Is close");
+        }
+    }
+
 
     public DriverDoc getDriverDoc() {
         return driverDoc;
